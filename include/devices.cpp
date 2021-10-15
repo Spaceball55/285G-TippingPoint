fb1022a16c6b99461bf6ba257279a904426982a8
David Rauch <Davespeed50@gmail.com>
2021-10-15 17:29:04 -0500

    Put in motorports

--- include/devices.cpp
+++ include/devices.cpp
@@ -2,8 +2,17 @@
 
 okapi::ChassisScales scales({3_in, 12_in}, imev5GreentTPR);
 
-okapi::MotorGroup movement = MotorGroup();
+okapi::MotorGroup movement = MotorGroup({10,2,-11,-20});
 
-okapi::MotorGroup inB = MotorGroup();
+//okapi::MotorGroup inB = MotorGroup();
 
-okapi::MotorGroup inF = MotorGroup();
+//okapi::MotorGroup inF = MotorGroup();
+
+/*
+std::shared_ptr<OdomChassisController> drive = 
+	ChassisControllerBuilder().withMotors(movement).withDimensions(okapi::AbstractMotor::gearset::green, scales).withOdometry(scales).buildOdometry();
+
+	std::shared_ptr<okapi::ChassisModel> model = std::dynamic_pointer_cast<okapi::ChassisModel>(drive->getModel());
+	
+	okapi::Controller controller;
+*/
