8075916dd9239e4579cd11aa37b7683591c2e4d1
David Rauch <Davespeed50@gmail.com>
2021-10-22 17:52:58 -0500

    Begin working with the tray

--- include/comp/devices.hpp
+++ include/comp/devices.hpp
@@ -4,4 +4,14 @@
 extern ChassissScales scales;
 extern std::shared_ptr<okapi::ChassisModel> model;
 
-extern std::shared_ptr<okapi::ChassisController> drive;
+extern std::shared_ptr<okapi::ChassisController> chassis;
+
+extern okapi::Motor belt;
+
+enum trayStates {
+	off,
+	up,
+	down
+};
+
+extern trayStates trayState;
